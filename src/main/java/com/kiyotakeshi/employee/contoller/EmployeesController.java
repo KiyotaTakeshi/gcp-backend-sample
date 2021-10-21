8b419b06045476035605b76dda2cbaa225c2324a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-10-21 09:25:40 +0900

    add response

--- src/main/java/com/kiyotakeshi/employee/contoller/EmployeesController.java
+++ src/main/java/com/kiyotakeshi/employee/contoller/EmployeesController.java
@@ -1,16 +1,25 @@
 package com.kiyotakeshi.employee.contoller;
 
+import com.kiyotakeshi.employee.entity.Employee;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
 
     @GetMapping
-    public ResponseEntity<?> getEmployee() {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<?> getEmployees() {
+        List<Employee> employeeList = new ArrayList(Arrays.asList(
+                new Employee("taro", "president"),
+                new Employee("jiro", "sales")
+        ));
+        return ResponseEntity.ok().body(employeeList);
     }
 }
--- src/test/java/com/kiyotakeshi/employee/controller/EmployeesControllerTest.java
+++ src/test/java/com/kiyotakeshi/employee/controller/EmployeesControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,6 +21,8 @@ public class EmployeesControllerTest {
     @Test
     void getEmployees() throws Exception {
         mockMvc.perform(get("/employees"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", is("taro")));
     }
 }
